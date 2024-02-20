8c33e47be219bceff6b15dff21dc4fd2dc1cf404
pwangy <blipsandclicks@gmail.com>
2024-02-20 10:20:49 -0800

    clean code

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,12 @@
-import React from "react";
-import blogData from "../data/blog";
+import blogData from '../data/blog'
 
-console.log(blogData);
+const App = () => {
+  console.log(blogData)
+	return (
+		<div className='App'>
+			You're on your own from here! Follow the deliverables; test things
+			out in the browser as you write your code; and good luck!
+		</div>
+)}
 
-function App() {
-  return (
-    <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
-    </div>
-  );
-}
-
-export default App;
+export default App
--- src/data/blog.js
+++ src/data/blog.js
@@ -1,31 +1,29 @@
-import logo from "../assets/logo";
+import logo from '../assets/logo'
 
 const blogData = {
-  name: "Underreacted",
-  image: logo,
-  about: "A blog about learning React",
-  posts: [
-    {
-      id: 1,
-      title: "Components 101",
-      date: "December 15, 2020",
-      preview: "Setting up the building blocks of your site",
-      minutes: 5,
-    },
-    {
-      id: 2,
-      title: "React Data Flow",
-      date: "December 11, 2020",
-      preview: "Passing props is never passé",
-      minutes: 15,
-    },
-    {
-      id: 3,
-      title: "Function vs Class Components",
-      preview: "React, meet OOJS.",
-      minutes: 47,
-    },
-  ],
-};
+	name: 'Underreacted',
+	image: logo,
+	about: 'A blog about learning React',
+	posts: [
+		{
+			id: 1,
+			title: 'Components 101',
+			date: 'December 15, 2020',
+			preview: 'Setting up the building blocks of your site',
+			minutes: 5
+		},
+		{
+			id: 2,
+			title: 'React Data Flow',
+			date: 'December 11, 2020',
+			preview: 'Passing props is never passé',
+			minutes: 15
+		},
+		{
+			id: 3,
+			title: 'Function vs Class Components',
+			preview: 'React, meet OOJS.',
+			minutes: 47
+}]}
 
-export default blogData;
+export default blogData
